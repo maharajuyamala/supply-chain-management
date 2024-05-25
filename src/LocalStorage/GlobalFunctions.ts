c5d4518d4032e53a94696c0c7114e9367ae9fa06
maharajuyamala <mahikkmp@gmail.com>
2024-05-25 10:52:48 +0530

    removed unused type

--- src/LocalStorage/GlobalFunctions.ts
+++ src/LocalStorage/GlobalFunctions.ts
@@ -1,9 +1,4 @@
-interface Item {
-  [key: string]: string | number | boolean | null | undefined; // Define the types for item properties
-}
-
 export const filteredItems = (data: any, searchQuery: string) => {
-  console.log(data);
   return searchQuery?.length !== 0 && searchQuery
     ? data.filter((item: string) =>
         Object.values(item).some(
